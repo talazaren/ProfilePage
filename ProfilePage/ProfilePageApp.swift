bddc3a01439b0055eaf7c3f9d3c00b2ddfe40f1f
Tatiana Lazarenko <tan.strebkova@gmail.com>
2026-01-22 16:16:43 +0300

    Initial Commit

---
+++ ProfilePage/ProfilePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePageApp.swift
+//  ProfilePage
+//
+//  Created by Tatiana Lazarenko on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProfilePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
